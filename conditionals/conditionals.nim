0efb678fcaff106454b765c072c5f31f4d9fc8c1
Stuart Zahn <stuart.zahn@gmail.com>
2014-05-26 17:07:51 -0700

    Working on switch case logic

--- conditionals/conditionals.nim
+++
@@ -1,6 +0,0 @@
-const sleep = "sleep"
-const eat = "eat"
-const drink = "drink"
-
-echo("What would you like to do?")
-let action = readLine()
--- switch_case/switch_case.nim
+++ switch_case/switch_case.nim
@@ -1,18 +1,38 @@
+from strutils import parseInt #For parsing integers from the input
+
+#Constants for verbs the user can choose
 const SLEEP = "sleep"
 const NAP = "nap"
 const EAT = "eat"
 const DRINK = "drink"
 
+#Ask the user what to do
 echo("What would you like to do?")
 let action = readLine(stdin)
+
+#Select the appropriate action
 case action
-of "":
-     echo("You want to do nothing")
-of SLEEP, NAP:
-     echo("You sleep soundly")
-of EAT:
-     echo("You have a delicious meal")
-of DRINK:
-     echo("You enjoy a cool beverage")
-else:
-     echo("Good luck with that!")
+of "": #User does nothing
+    echo("You want to do nothing")
+of SLEEP, NAP: #User sleeps
+    echo("You sleep soundly for an hour. Keep sleeping? (Y/N)")
+    var keepSleeping = readLine(stdin)
+    while (keepSleeping == "Y"): #keep sleeping for an hour until user stops
+        echo("You sleep for another hour. Keep sleeping? (Y/N)")
+        keepSleeping = readLine(stdin)
+of EAT: #User eats
+    echo("You have a delicious meal")
+of DRINK: #User drinks
+    echo("How many drinks would you like to have?")
+    let drinks = parseInt(readLine(stdin))
+    case drinks #Drink moderately
+        of 0: echo ("You don't drink anything")
+        of 1..3:
+            for i in countup(1, drinks): #Count number of cans drunk
+                let canNoun = if i == 1 : " can" else: " cans"
+                echo ("You have chuggged ", i, canNoun)
+        else: echo("That's one too many drinks for you")
+else: #An action was typed that was not supported
+    echo("Good luck with that!")
+
+echo("You go home")
